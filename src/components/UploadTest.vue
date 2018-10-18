cb3529d06c4df101dddf86a985a8b4f532e4e6a2
Unknown <lpreterite@126.com>
2018-10-18 15:38:43 +0800

    add example with qiniu-uploader version 1.0.0

--- src/components/UploadTest.vue
+++ src/components/UploadTest.vue
@@ -1,40 +1,88 @@
 <template>
+<div>
+    <p>
+        <label>七牛token：<input type="text" v-model="token" style="width: 400px"></label>
+    </p>
     <div>
-        <button @click="openFinder(onFileChanged)">Upload</button>
-        <button @click="cancel">Cancel</button>
+        <div>
+            <button type="button" @click="uploader.openFinder(onFileChanged, {multiple: true})">选择文件</button>
+        </div>
+        <ul class="progress">
+            <li v-for="(progress, index) in fileset" :key="index">
+                <div class="progress__title">
+                    {{ progress.blob.name }}<small>（{{ progress.fileKey }}）</small>
+                    <span>
+                        <button type="button" v-if="!progress.uploading && progress.stage !== 'cancel'" @click="upload(progress.fileKey)">上传</button>
+                        <button type="button" v-if="progress.blockCount>1 && progress.uploading" @click="uploader.cancel(progress.fileKey)">暂停</button>
+                        <button type="button" v-if="progress.blockCount>1 && progress.stage === 'cancel'" @click="upload(progress.fileKey)">继续</button>
+                    </span>
+                </div>
+                <div class="progress__meta" v-if="progress.uploading">上传进度：{{progress.value}}% - {{progress.loaded}}kb / {{progress.total}}kb —— 当前块：{{progress.blockIndex}} 当前阶段：{{progress.stage}}</div>
+                <div class="progress__meta" v-if="!progress.uploading && progress.errors.length > 0">{{ progress.errors[0].message }}</div>
+            </li>
+        </ul>
     </div>
+</div>
 </template>
 
 <script>
 import uploader from '@packy-tang/qiniu-uploader';
-const token = "fqW-wERKESARsZ9_8-sZCvRhLNqNOIgC3ijJsG-9:Bd3jJ9JgcOIpGhJHJDi6RHHdIic=:eyJzY29wZSI6Imhla2F0ZSIsImRlYWRsaW5lIjoxNTM5Njg1Nzc3fQ=="
-
 export default {
+    data(){
+        return {
+            uploader,
+            token: "fqW-wERKESARsZ9_8-sZCvRhLNqNOIgC3ijJsG-9:SN4ozAQViWGzQI60IykeC5MEbmo=:eyJzY29wZSI6Imhla2F0ZSIsImRlYWRsaW5lIjoxNTM5ODQ2NjkyfQ==",
+            loading: false,
+            fileset: []
+        }
+    },
+    mounted(){
+        uploader.init({
+            onChanged: ()=>{
+                this.fileset = uploader.getAllFile()
+            }
+        })
+    },
     methods: {
         openFinder(fn){
             return uploader.openFinder(fn)
         },
-        async onFileChanged(files, uploader){
-            console.time('GetFileKey')
-            const fileKey = await uploader.getFileKey(files[0])
-            console.timeEnd('GetFileKey')
-
-            uploader.upload(fileKey, files[0], {
-                url: '//up-z2.qiniup.com',
-                token,
-                onUploadProgress: ({fileKey, loaded, total, progressStage, progressValue, blockIndex})=>{
-                    total = Math.floor((total || 0) / 1024);
-                    loaded = Math.floor((loaded || 0) / 1024);
-                    console.log(`上传进度：${progressValue}% - ${loaded}kb / ${total}kb —— 当前块：${blockIndex} 当前阶段：${progressStage}`);
-                },
-                onFail:(errors, {isCancel})=>{
-                    if(isCancel) console.log(errors)
-                }
+        onFileChanged(files, uploader){
+            Array.prototype.forEach.call(files, async (blob, index)=>{
+                const fileKey = await uploader.getFileKey(blob)
+                uploader.addFile(fileKey, blob)
             })
         },
-        cancel(){
-            uploader.cancel()
+        upload(fileKey){
+            this.uploader.upload(fileKey, { token:this.token, url: "//up-z2.qiniup.com" })
+        },
+        cancel(key){
+            uploader.cancel(key)
         }
     }
 }
 </script>
+
+<style>
+.progress{
+    text-align: left;
+    width: 80%;
+    margin: 35px auto;
+    list-style: none;
+}
+.progress__title{
+    margin: 10px 0;
+}
+.progress__title small{
+    color: #999;
+}
+.progress__meta{
+    font-size: small;
+    padding-left: 1em;
+    margin: 10px 0 20px;
+}
+.progress__meta:before{
+    content: "-";
+    margin-right: 1em;
+}
+</style>
